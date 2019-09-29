d7428e7387ed76789423031f04a7f6de86c37590
Zhuel Itua <ituazhuel@hotmail.com>
2019-09-29 17:08:37 +0300

    (dev-4) restore MvvmFrame.Wpf.TestWpf project

---
+++ MvvmFrame.Wpf/MvvmFrame.Wpf.TestWpf/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmFrame.Wpf.TestWpf.Pages;
+using MvvmFrame.Wpf.TestWpf.ViewModels;
+using System.Windows;
+
+namespace MvvmFrame.Wpf.TestWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModelBase.Navigate<HomePage>(
+                ViewModelBase.CreateViewModel<HomeViewModel>(_mainFrame));
+        }
+    }
+}
---
+++ MvvmFrame.Wpf/MvvmFrame.Wpf.TestWpf/Pages/HomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MvvmFrame.Interfaces;
+using System.Windows.Controls;
+
+namespace MvvmFrame.Wpf.TestWpf.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page, IPage
+    {
+        public void InitializePageComponent<TViewModel>(TViewModel viewModel) where TViewModel : IViewModel
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
