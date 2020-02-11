bcb8d56af6f3a6d5bfe4afeee732f63f5f4f6a32
GetcuReone <getcureone@hotmail.com>
2020-02-11 20:45:30 +0300

    Restore tests

--- MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.UnitTests/Commands/FirstCanCommandTests.cs
+++ MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.UnitTests/Commands/FirstCanCommandTests.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 namespace MvvmFrame.Wpf.UnitTests.Commands
 {
     [TestClass]
-    [Ignore]
     public class FirstCanCommandTests
     {
         [Timeout(Timeuots.Millisecond.Twenty)]
