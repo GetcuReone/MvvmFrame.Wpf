b22966fb1fb769ae14c2ff5eca23d170d9de2253
GetcuReone <getcureone@hotmail.com>
2020-02-15 17:52:03 +0300

    fix project

--- MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.UnitTests/Properties/AssemblyInfo.cs
+++ MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.UnitTests/Properties/AssemblyInfo.cs
@@ -13,7 +13,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("042b463f-ecc0-4203-b10d-9b7256b61e9c")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
