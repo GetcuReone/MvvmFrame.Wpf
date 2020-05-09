ef15bafce4ead2c6149ddca9aca867089d0a515a
Itua Jeul <GetcuReone@hotmail.com>
2020-05-09 17:13:44 +0300

    #2 fix test

--- MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.Tests/UiServices/UiServicesTests.cs
+++ MvvmFrame.Wpf/Tests/MvvmFrame.Wpf.Tests/UiServices/UiServicesTests.cs
@@ -160,7 +160,7 @@ namespace MvvmFrame.Wpf.Tests.UiServices
 
                 TestHelper.ExpectedException<ArgumentException>(
                     () => ViewModel.UiServices.AddSingleton<UiService, UiService>(_frame),
-                    "service already added previously");
+                    "Service already added previously.");
             }, Timeuots.Second.One);
         }
 
